08385abd7d3388d4b5b4db2bd7d8dd4fdb81e054
mikhaelHan <hancharuk270185@mail.ru>
2024-01-29 12:58:34 +0300

    feat: add src/zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import { error } from 'console';
+import zlib from 'zlib';
+
+const __dirname = import.meta.dirname;
+
+const readStream = fs.createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+const writeStream = fs.createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+const compressStream = zlib.createGzip();
+
+
+const handlerError = () => {
+    console.log('TRANSFORM peration failed...');
+    readStream.destroy();
+    writeStream.end('Finished with Eror...')
+}
+
 const compress = async () => {
-    // Write your code here 
+    readStream
+    .on('error', handlerError)
+    .pipe(compressStream)
+    .pipe(writeStream)
+    .on('error', handlerError);
 };
 
-await compress();
+try {
+    await compress();
+} catch (err) {
+    error(`ERROR: ${err.message}`);
+}
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import { error } from 'console';
+import zlib from 'zlib';
+
+const __dirname = import.meta.dirname;
+
+const readStream = fs.createReadStream(path.join(__dirname, 'files', 'archive.gz'));
+const writeStream = fs.createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+const decompressStream = zlib.createGunzip();
+
+const handlerError = () => {
+    console.log('DECOMPRESS operation failed...');
+    readStream.destroy();
+    writeStream.end('Finished with Error...')
+}
+
 const decompress = async () => {
-    // Write your code here 
+    readStream
+    .on('error', handlerError)
+    .pipe(decompressStream)
+    .pipe(writeStream)
+    .on('error', handlerError);
 };
 
-await decompress();
+try {
+    await decompress();
+} catch (err) {
+    error(`ERROR: ${err.message}`);
+}
